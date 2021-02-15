88ea36defc0765d531d271fda9c8b3059e56cb93
Martin Hastrup <Martin Hastrup>
2021-02-15 19:57:13 +0100

    back again

--- robot_main.py
+++ robot_main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     in2 = 23
     in3 = 26
     in4 = 16
-    #en = 25
+    en = 25
     temp1 = 1
 
     GPIO.setmode(GPIO.BCM)
@@ -35,14 +35,14 @@ if __name__ == "__main__":
     GPIO.setup(in2, GPIO.OUT)
     GPIO.setup(in3, GPIO.OUT)
     GPIO.setup(in4, GPIO.OUT)
-    #GPIO.setup(en, GPIO.OUT)
+    GPIO.setup(en, GPIO.OUT)
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
-    #p = GPIO.PWM(en, 100)
+    p = GPIO.PWM(en, 100)
 
-    #p.start(25)
+    p.start(25)
     print("\n")
     print("The default speed & direction of motor is LOW & Forward.....")
     print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")

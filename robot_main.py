ff5c978d884daf2fa4cbd5da475310ed3f424c12
Martin Hastrup <Martin Hastrup>
2021-02-15 17:31:43 +0100

    new controll

--- robot_main.py
+++ robot_main.py
@@ -3,5 +3,32 @@ import numpy as np
 # pi IP: 192.168.1.58
 # RUN ssh pi@192.168.1.58
 
+import RPi.GPIO as GPIO
+from time import sleep
+
 if __name__ == "__main__":
-    print('Hello World!')
+    in1 = 24
+    in2 = 23
+    en = 25
+    temp1 = 1
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    GPIO.setup(en, GPIO.OUT)
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    p = GPIO.PWM(en, 1000)
+
+    p.start(25)
+    print("\n")
+    print("The default speed & direction of motor is LOW & Forward.....")
+    print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+    print("\n")
+
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+
+    sleep(2)
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)

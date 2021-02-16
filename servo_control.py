32765e60525e63d195acb59cc770d92cdb711280
Martin Hastrup <Martin Hastrup>
2021-02-16 21:37:31 +0100

    adjsuting servo

--- servo_control.py
+++ servo_control.py
@@ -4,13 +4,13 @@ import time
 kit = ServoKit(channels=16)
 
 
-kit.servo[0].angle = 45
+kit.servo[0].angle = 90
 time.sleep(1)
 kit.servo[0].angle = 0
 time.sleep(1)
-kit.servo[0].angle = 90
+kit.servo[0].angle = 180
 time.sleep(1)
-kit.servo[0].angle = 45
+kit.servo[0].angle = 90
 time.sleep(1)
 
 kit.servo[1].angle = 0

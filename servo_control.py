a3f2416899299a3e58ff173d54759de6a6254ec0
Martin Hastrup <Martin Hastrup>
2021-02-16 17:51:01 +0100

    servo power

--- servo_control.py
+++ servo_control.py
@@ -25,8 +25,8 @@ def set_servo_pulse(channel, pulse):
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-pwm.set_pwm(0, 0, 375)
-pwm.set_pwm(1, 0, 375)
+pwm.set_pwm(0, 0, 275)
+pwm.set_pwm(1, 0, 275)
 """
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:

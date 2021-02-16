b5a8c0f5b7dd92c727dc6ee242693779cad5b043
Martin Hastrup <Martin Hastrup>
2021-02-16 19:30:26 +0100

    sleeping servo

--- servo_control.py
+++ servo_control.py
@@ -1,14 +1,23 @@
 from adafruit_servokit import ServoKit
+import time
 
 kit = ServoKit(channels=16)
 
 
-kit.servo[0].angle = -90
+kit.servo[0].angle = 0
+time.sleep(1)
 kit.servo[0].angle = 90
-kit.servo[0].angle = -90
+time.sleep(1)
+kit.servo[0].angle = 0
+time.sleep(1)
 kit.servo[0].angle = 90
+time.sleep(1)
 
-kit.servo[1].angle = -45
+kit.servo[1].angle = 0
+time.sleep(1)
 kit.servo[1].angle = 45
-kit.servo[1].angle = -45
+time.sleep(1)
+kit.servo[1].angle = 0
+time.sleep(1)
 kit.servo[1].angle = 45
+time.sleep(1)

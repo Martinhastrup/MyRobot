f3c35797d7109b5b76347de6b64a01fda656adb7
Martin Hastrup <Martin Hastrup>
2021-02-16 19:32:17 +0100

    back and forth

--- servo_control.py
+++ servo_control.py
@@ -3,21 +3,21 @@ import time
 
 kit = ServoKit(channels=16)
 
-
-kit.servo[0].angle = 0
-time.sleep(1)
 kit.servo[0].angle = 90
 time.sleep(1)
 kit.servo[0].angle = 0
 time.sleep(1)
 kit.servo[0].angle = 90
 time.sleep(1)
+kit.servo[0].angle = 0
+time.sleep(1)
 
-kit.servo[1].angle = 0
-time.sleep(1)
 kit.servo[1].angle = 45
 time.sleep(1)
 kit.servo[1].angle = 0
 time.sleep(1)
 kit.servo[1].angle = 45
 time.sleep(1)
+kit.servo[1].angle = 0
+time.sleep(1)
+

b1c28be10b8bfe1204aafbd191638a412aa78d46
Martin Hastrup <Martin Hastrup>
2021-02-18 17:06:31 +0100

    adjust motor

--- skynet.py
+++ skynet.py
@@ -31,10 +31,10 @@ GPIO.setup(M2_forward, GPIO.OUT)
 GPIO.setup(M2_backward, GPIO.OUT)
 GPIO.setup(en1, GPIO.OUT)
 GPIO.setup(en2, GPIO.OUT)
-p1 = GPIO.PWM(en1, 60) # left wheel?
+p1 = GPIO.PWM(en1, 58) # left wheel?
 p2 = GPIO.PWM(en2, 50) # right wheel?
 
-p1.start(60)
+p1.start(58)
 p2.start(50)
 
 

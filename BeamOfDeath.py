9c8c041de9cbb161311be7574fc21fd2b5f3404e
Martin Hastrup <Martin Hastrup>
2021-02-23 16:46:13 +0100

    always on

--- BeamOfDeath.py
+++ BeamOfDeath.py
@@ -10,8 +10,10 @@ GPIO.setup(laser_out, GPIO.OUT)
 try:
     while True:
         GPIO.output(laser_out, GPIO.HIGH)
+        print('Beam of Death ON')
         time.sleep(2)
         GPIO.output(laser_out, GPIO.LOW)
+        print('Beam of Death OFF')
         time.sleep(2)
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:

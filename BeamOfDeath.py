2cdc9c1c70177aa14800f7343dacaa739149ad62
Martin Hastrup <Martin Hastrup>
2021-02-23 16:41:49 +0100

    adding laser

---
+++ BeamOfDeath.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+
+laser_out = 5
+try:
+    while True:
+        GPIO.output(laser_out, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(laser_out, GPIO.LOW)
+        time.sleep(2)
+
+
+# If you press CTRL+C, cleanup and stop
+except KeyboardInterrupt:
+stop()
+GPIO.cleanup()
+print("Exiting")

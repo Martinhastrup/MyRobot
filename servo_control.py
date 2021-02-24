15ab8348d1c2d6659ed8c1fd74b1db7eb29615d5
Martin Hastrup <Martin Hastrup>
2021-02-24 21:45:55 +0100

    adjusting

--- servo_control.py
+++ servo_control.py
@@ -5,31 +5,31 @@ kit = ServoKit(channels=16)
 
 
 def go_up():
-    for i in range(9):
-        kit.servo[1].angle = 45 - (i*5)
+    for i in range(18):
+        kit.servo[1].angle = 45 - (i*2)
         time.sleep(0.05)
 
 
 def go_down():
-    for i in range(9):
-        kit.servo[1].angle = 45 + (i*5)
+    for i in range(18):
+        kit.servo[1].angle = 45 + (i*2)
         time.sleep(0.05)
 
 
 def go_left():
-    for i in range(9):
-        kit.servo[0].angle = 45 + (i*5)
+    for i in range(18):
+        kit.servo[0].angle = 45 + (i*2)
         time.sleep(0.05)
 
 
 def go_right():
-    for i in range(9):
-        kit.servo[0].angle = 45 - (i*5)
+    for i in range(18):
+        kit.servo[0].angle = 45 - (i*2)
         time.sleep(0.05)
 
 def reset():
-    kit.servo[0].angle = 0
-    kit.servo[1].angle = 0
+    kit.servo[0].angle = 45
+    kit.servo[1].angle = 45
 
 #kit.servo[0].angle = 90
 #time.sleep(1)

6ef543d9f54607f533eddcd8814922323b21935e
Martin Hastrup <Martin Hastrup>
2021-02-24 21:44:25 +0100

    reset angle

--- servo_control.py
+++ servo_control.py
@@ -7,25 +7,29 @@ kit = ServoKit(channels=16)
 def go_up():
     for i in range(9):
         kit.servo[1].angle = 45 - (i*5)
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 
 def go_down():
     for i in range(9):
         kit.servo[1].angle = 45 + (i*5)
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 
 def go_left():
     for i in range(9):
         kit.servo[0].angle = 45 + (i*5)
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 
 def go_right():
     for i in range(9):
         kit.servo[0].angle = 45 - (i*5)
-        time.sleep(0.1)
+        time.sleep(0.05)
+
+def reset():
+    kit.servo[0].angle = 0
+    kit.servo[1].angle = 0
 
 #kit.servo[0].angle = 90
 #time.sleep(1)
@@ -40,3 +44,4 @@ go_left()
 go_right()
 go_up()
 go_down()
+reset()

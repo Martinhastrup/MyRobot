10ab48be24e8a3e2296a9f3dfed4a296de4a8b76
Martin Hastrup <Martin Hastrup>
2021-02-25 09:10:33 +0100

    adjusting engine

--- skynet.py
+++ skynet.py
@@ -42,10 +42,10 @@ GPIO.setup(M2_backward, GPIO.OUT)
 GPIO.setup(en1, GPIO.OUT)
 GPIO.setup(en2, GPIO.OUT)
 p1 = GPIO.PWM(en1, 60) # left wheel
-p2 = GPIO.PWM(en2, 56) # right wheel
+p2 = GPIO.PWM(en2, 50) # right wheel
 
 p1.start(60)
-p2.start(56)
+p2.start(50)
 
 
 def distance():

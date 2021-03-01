44dbfdf51eb1c5c606c87081788bc9814cd7782e
Martin Hastrup <Martin Hastrup>
2021-03-01 17:52:55 +0100

    more power

--- skynet.py
+++ skynet.py
@@ -46,8 +46,8 @@ GPIO.setup(en2, GPIO.OUT)
 p1 = GPIO.PWM(en1, 100)  # right wheel
 p2 = GPIO.PWM(en2, 100)  # left wheel
 
-p1.start(65)
-p2.start(65)
+p1.start(85)
+p2.start(85)
 
 
 def distance():

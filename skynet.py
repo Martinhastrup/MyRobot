f24802ca294d7c2d6ac75e97bcf6ab5c707538c4
Martin Hastrup <Martin Hastrup>
2021-03-02 10:19:43 +0100

    test turn

--- skynet.py
+++ skynet.py
@@ -44,8 +44,8 @@ GPIO.setup(M2_forward, GPIO.OUT)
 GPIO.setup(M2_backward, GPIO.OUT)
 GPIO.setup(en1, GPIO.OUT)
 GPIO.setup(en2, GPIO.OUT)
-p1 = GPIO.PWM(en1, 1000)  # right wheel
-p2 = GPIO.PWM(en2, 1000)  # left wheel
+p1 = GPIO.PWM(en1, 50)  # right wheel
+p2 = GPIO.PWM(en2, 50)  # left wheel
 
 p1.start(0)
 p2.start(0)

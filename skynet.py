b7f17ebc4daed2e65b2f8f6ba003b97700ce69f6
Martin Hastrup <Martin Hastrup>
2021-03-16 21:55:35 +0100

    no carpet

--- skynet.py
+++ skynet.py
@@ -42,8 +42,8 @@ backward_M2 = GPIO.PWM(M2_backward, 100)
 p1 = GPIO.PWM(en1, 100)  # right wheel
 p2 = GPIO.PWM(en2, 100)  # left wheel
 
-p1.start(100)
-p2.start(100)
+p1.start(75)
+p2.start(75)
 
 
 def init():

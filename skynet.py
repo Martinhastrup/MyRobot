dc12c200f3d333e18066c8e281fceecf85011d9f
Martin Hastrup <Martin Hastrup>
2021-03-16 21:43:29 +0100

    reduce power

--- skynet.py
+++ skynet.py
@@ -42,8 +42,8 @@ backward_M2 = GPIO.PWM(M2_backward, 100)
 p1 = GPIO.PWM(en1, 100)  # right wheel
 p2 = GPIO.PWM(en2, 100)  # left wheel
 
-p1.start(80)
-p2.start(80)
+p1.start(50)
+p2.start(50)
 
 
 def init():
